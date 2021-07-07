1f84161e07b93d189bac8d7d091800d1c8f5deb6
Abhijeet0001 <64309773+Abhijeet0001@users.noreply.github.com>
2021-07-07 16:53:45 +0530

    Add files via upload

---
+++ Screens/CreateStory.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+import { TextBase,View } from 'react-native'
+export default class CreateStory extends Component {
+    render(){
+        return(<View>
+            <Text> Story </Text>
+        </View>)
+    }
+}
